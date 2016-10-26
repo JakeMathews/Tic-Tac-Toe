6718a217143a7a223bd9ded27d03abf12afa6738
Jake Mathews <mathewsj2@wit.edu>
2016-10-26 10:41:38 -0400

    Added bootstrap

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 let http = require("http");
-let fs = require("fs");
+//let fs = require("fs");
+let path = require("path");
+let express = require("express");
+let app = express();
 
-var server = http.createServer(function(req, res) {
-  
+//var router = express.Router([options]);
+
+app.use('/static', express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile("index.html", { root: path.join(__dirname, "/public")});
 });
 
-server.listen(8000);
+app.listen(8000, function () {
+  console.log("Tic-Tac-Toe server listening on port 8000!");
+});
