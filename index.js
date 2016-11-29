df6df9645564fdfbc8c07d50b31d3aaaa3f79354
Jake Mathews <mathewsj2@wit.edu>
2016-11-28 23:54:18 -0500

    Added body parser and a route for starting a game

--- index.js
+++ index.js
@@ -2,16 +2,41 @@ let http = require("http");
 //let fs = require("fs");
 let path = require("path");
 let express = require("express");
+let bodyParser = require("body-parser");
 let app = express();
 
 //var router = express.Router([options]);
 
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({
+	extended: false
+})
+
 app.use('/static', express.static('public'));
 
-app.get('/', function (req, res) {
-  res.sendFile("index.html", { root: path.join(__dirname, "/public")});
+app.get('/', function(req, res) {
+	res.sendFile("index.html", {
+		root: path.join(__dirname, "/public")
+	});
 });
 
-app.listen(8000, function () {
-  console.log("Tic-Tac-Toe server listening on port 8000!");
+app.post('/create', urlencodedParser, function(req, res) {
+	// Recieve user data to create game here
+	res.status(200).send({
+		location: "create"
+	});
+});
+
+app.get("/create", function(req, res) {
+	// Create the game and send the game file
+	res.sendFile("game.html", {
+		root: path.join(__dirname, "/public")
+	});
+});
+
+app.listen(8000, function() {
+	console.log("Tic-Tac-Toe server listening on port 8000!");
 });
